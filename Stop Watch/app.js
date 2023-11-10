72b695eb489c1308a83717f9141f507ba525b296
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-11-10 13:08:34 +0500

    making stop watch

--- Stop Watch/app.js
+++ Stop Watch/app.js
@@ -2,15 +2,18 @@ var hr = 0;
 var min = 0;
 var sec = 0;
 var msec = 0;
+var a = document.getElementById('msec')
+var b = document.getElementById('sec')
+var c = document.getElementById('min')
+var d = document.getElementById('hr')
 
 function start(){
     setInterval(function(){
-        
-        var a = document.getElementById('msec')
-        var b = document.getElementById('sec')
-        var c = document.getElementById('min')
-        var d = document.getElementById('hr')
+        msec++       
         a.innerHTML = msec;
-        
-    },1000)
+        if(msec >= 100){
+            sec++
+            b.innerHTML = sec;
+        }
+    },10)
 }
